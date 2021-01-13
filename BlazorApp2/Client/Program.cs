6be53c333f673d0877106015f945ca959abd2e1a
oguz687 <oguzhan_687@hotmail.com>
2021-01-14 00:57:25 +0300

    65

--- BlazorApp2/Client/Program.cs
+++ BlazorApp2/Client/Program.cs
@@ -20,6 +20,7 @@ namespace BlazorApp2.Client
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
+            //uykuyukyukyukyu
         }
     }
 }
